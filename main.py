696a22698c52361f33fc9e39f72ff59478eb907e
mjuhaNotebook <m.juhaniak17@gmail.com>
2022-03-28 15:00:34 +0200

    Added argument and coefficient in method for dataset preparation. Commented methods

--- main.py
+++ main.py
@@ -1,68 +1,64 @@
-
 from scipy.stats import entropy
 import pandas as pd
 
-PaloAlto = pd.read_csv(r"C:\Users\Martin\Projekt\PaloAlto_modified.csv", low_memory=False)
-SD = 0
-DE = 0
-SD_sparsity = 0
-DE_sparsity = 0
+
+# Creates a new column in dataset with a desired format,
+# set argument to 1 if column´s type is datetime,
+# set argument to 2 if column´s type is int,
+# coefficient is optional parameter for changing units of int(if argument = 2)
+def prepare_data(dataset, column_name, column_name_new, argument, coefficient=1):
+    if argument == 1:
+        dataset[column_name_new] = pd.to_datetime(dataset[column_name]).dt.hour
+    elif argument == 2:
+        dataset[column_name_new] = round(dataset[column_name] * coefficient)
 
 
-def computeSparsity():
-    global SD_sparsity, DE_sparsity
+# Returns a 2D matrix from 2 columns with number of occurences
+def create_sparsity_matrix(dataset, column_name1, column_name2):
+    sparsity_matrix = dataset.groupby(column_name1)[column_name2].value_counts().unstack(column_name2,
+                                                                                         fill_value=0).reset_index()
+    sparsity_matrix.drop(sparsity_matrix.columns[0], axis=1, inplace=True)
+    sparsity_matrix.to_csv("sparsityMatrix.csv", sep=';')
+    return sparsity_matrix
 
-    SD_zeros = (SD == 0).sum().sum()
-    DE_zeros = (DE == 0).sum().sum()
 
-    SD_sparsity = SD_zeros/(SD.shape[0]*SD.shape[1])
-    DE_sparsity = DE_zeros/(DE.shape[0]*DE.shape[1])
+# Returns sparsity of a matrix
+def compute_sparsity(sparsity_matrix):
+    matrix_zeros = (sparsity_matrix == 0).sum().sum()
+    sparsity = matrix_zeros / (sparsity_matrix.shape[0] * sparsity_matrix.shape[1])
+    return sparsity
 
 
-def getEntropy():
-    global PaloAlto, SD, DE
-
-    counts = SD.to_numpy().flatten(order='C')
+# Returns entropy of dataset based on sparsity matrix
+def compute_entropy(sparsity_matrix):
+    counts = sparsity_matrix.to_numpy().flatten(order='C')
     return entropy(counts, base=2)
 
 
-def prepareData():
-    global PaloAlto
-
-    PaloAlto['StartTime'] = pd.to_datetime(PaloAlto['Start Date']).dt.hour
-    PaloAlto['Duration'] = PaloAlto['Charging Time (hh:mm:ss)'] / 3600
-    PaloAlto['Energy'] = PaloAlto['Energy (kWh)']
-    PaloAlto = PaloAlto.round({'Duration': 0, 'Energy': 0})
-
-
-def createSparsityMatrix():
-    global PaloAlto, SD, DE
-    SD = PaloAlto.groupby('StartTime')['Duration'].value_counts().unstack('Duration', fill_value=0).reset_index()
-    DE = PaloAlto.groupby('Duration')['Energy'].value_counts().unstack('Energy', fill_value=0).reset_index()
-
-    SD.drop(SD.columns[0], axis=1, inplace=True)
-    DE.drop(DE.columns[0], axis=1, inplace=True)
-
-    SD.to_csv("SD_sparsity.csv", sep=';')
-    DE.to_csv("DE_sparsity.csv", sep=';')
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    prepareData()
-    createSparsityMatrix()
-    computeSparsity()
-    print(SD_sparsity)
-    print(DE_sparsity)
-    print(getEntropy())
+    dataset_ = pd.read_csv(r"C:\Users\mjuha\Desktop\Prax_data\Data\PaloAlto_modified.csv", low_memory=False)
+    column1_name_ = 'Start Date'
+    column2_name_ = 'Charging Time (hh:mm:ss)'
+    column1_name_new_ = 'NewCol1'
+    column2_name_new_ = 'NewCol2'
 
+    prepare_data(dataset_, column1_name_, column1_name_new_, 1)
+    prepare_data(dataset_, column2_name_, column2_name_new_, 2, 1/3600)
+    sparsity_matrix_ = create_sparsity_matrix(dataset_, column1_name_new_, column2_name_new_)
+    sparsity_ = compute_sparsity(sparsity_matrix_)
+    entropy_ = compute_entropy(sparsity_matrix_)
+    print('SD:')
+    print(sparsity_)
+    print(entropy_)
 
-    print("--------")
-    list = [[3, 0, 0], [0, 2, 0], [0, 0, 1]]
-    dframe = pd.DataFrame(list)
-    print(dframe)
-    flattenedframe = dframe.to_numpy().flatten(order='C')
-    print(flattenedframe)
-    print(entropy(flattenedframe, base=2))
+    column1_name_ = 'Charging Time (hh:mm:ss)'
+    column2_name_ = 'Energy (kWh)'
 
-
+    prepare_data(dataset_, column1_name_, column1_name_new_, 2, 1/3600)
+    prepare_data(dataset_, column2_name_, column2_name_new_, 2)
+    sparsity_matrix_ = create_sparsity_matrix(dataset_, column1_name_new_, column2_name_new_)
+    sparsity_ = compute_sparsity(sparsity_matrix_)
+    entropy_ = compute_entropy(sparsity_matrix_)
+    print('DE:')
+    print(sparsity_)
+    print(entropy_)
